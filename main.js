d71d3095495433fef133500e7e58d5bfdb71035f
nairajunk10-glitch <nairajunk10@gmail.com>
2026-03-29 04:15:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,67 @@
 function showPeriode(id) {
-    const section = document.getElementById("kegiatan");
-  
-    section.querySelectorAll(".periode").forEach(el => {
-      el.classList.remove("aktif");
-    });
-  
-    section.querySelector("#" + id).classList.add("aktif");
-  }
+  const section = document.getElementById("kegiatan");
 
-  function showStruktur() {
-    const selected = document.getElementById("pilihStruktur").value;
-  
-    document.querySelectorAll(".struktur-periode").forEach(el => {
-      el.classList.remove("aktif");
-    });
-  
-    if (selected) {
-      document.getElementById(selected).classList.add("aktif");
-    }
+  section.querySelectorAll(".periode").forEach(el => {
+    el.classList.remove("aktif");
+  });
+
+  section.querySelector("#" + id).classList.add("aktif");
+}
+
+function showStruktur() {
+  const selected = document.getElementById("pilihStruktur").value;
+
+  document.querySelectorAll(".struktur-periode").forEach(el => {
+    el.classList.remove("aktif");
+  });
+
+  if (selected) {
+    document.getElementById(selected).classList.add("aktif");
   }
+}
+function toggleMenu() {
+  document.querySelector(".nav-links").classList.toggle("active");
+}
+  let index = 0;
+
+  function nextSlide() {
+    const total = document.querySelectorAll(".profil-card").length;
+  
+    if (index < total - 1) {
+      index++;
+    }
+  
+    console.log("NEXT:", index);
+    updateSlide();
+  }
+  
+  function prevSlide() {
+    if (index > 0) {
+      index--;
+    }
+  
+    console.log("PREV:", index);
+    updateSlide();
+  }
+  
+  function updateSlide() {
+    const slider = document.querySelector(".profil-grid");
+    slider.style.transform = `translateX(-${index * 100}%)`;
+  }
+ 
+function hideArrowsOnDesktop() {
+const arrows = document.querySelectorAll('.arrow');
+if (window.innerWidth > 768) {
+    arrows.forEach(arrow => {
+        arrow.style.display = 'none';
+    });
+} else {
+    arrows.forEach(arrow => {
+        arrow.style.display = 'flex'; 
+    });
+}
+}
+
+hideArrowsOnDesktop();
+
+window.addEventListener('resize', hideArrowsOnDesktop);
